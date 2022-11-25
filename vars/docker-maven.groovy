3d4b398b60f06ac796ec2fb7883451947f7d3ac9
mvpc-11232 <mvp2612@hotmail.com>
2022-11-25 07:33:50 +0530

    Auto Push Commit

--- vars/docker-maven.groovy
+++ vars/docker-maven.groovy
@@ -1,1 +1,15 @@
-docker-maven
+def call() {
+    node {
+        sh 'rm -rf *'
+        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
+        
+        stage('Image Build') {
+            sh "mvn clean package"
+            sh "mv target/shipping-1.0.jar shipping.jar"
+        }
+        stage('Docker Build') {
+            sh "docker build ."
+            sh "docker images"
+        }
+    }
+}
