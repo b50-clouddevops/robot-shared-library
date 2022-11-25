bee1a722482aff35b187dfbcfe3e250aabc3a21d
mvpc-11232 <mvp2612@hotmail.com>
2022-11-25 06:45:02 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,13 +1,13 @@
-def call() {
-    node {
-        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
-        env.APPTYPE="nodejs"
-        common.sonarCheck()
-        common.lintCheck()
-        env.ARGS="-Dsonar.sources=."
-        common.testCases()
-        if (env.TAG_NAME != null) {
-            common.artifact()
-        }
-    }
-}
+// def call() {
+//     node {
+//         sh 'rm -rf *'
+//         git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
+//         common.sonarCheck()
+//         common.lintCheck()
+//         env.ARGS="-Dsonar.sources=."
+//         common.testCases()
+//         if (env.TAG_NAME != null) {
+//             common.artifact()
+//         }
+//     }
+// }
