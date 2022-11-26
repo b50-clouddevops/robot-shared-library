56d6481ac7fae871e50afc82d3c147c2d7ad057d
mvpc-11232 <mvp2612@hotmail.com>
2022-11-26 06:13:14 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -25,7 +25,7 @@ def call() {
         stage('Docker Push') {
             sh "docker tag 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest  355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
             sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 355449129696.dkr.ecr.us-east-1.amazonaws.com"
-            sh "docker push 355449129696.dkr.ecr.us-east-1.amazonaws.com/frontend:latest"
+            sh "docker push 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
         }
     }
 }
