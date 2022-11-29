389fd75ba437dd0950d77314fa41d1f210d99bb7
mvpc-11232 <mvp2612@hotmail.com>
2022-11-29 06:24:10 +0530

    Auto Push Commit

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,43 +4,16 @@ def call() {
    parameters {
         string(name: 'VERSION', defaultValue: '', description: 'Chose the version to deploy')
         string(name: 'ENV', defaultValue: 'dev', description: 'Chose the ENV to deploy')
-        booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
     }
         stages {
-            stage('Lint Check') {
+            stage('Deploying Component') {
                 steps {
-                    script { 
-                        lintCheck()
-                    }
+                    sh "echo Authenticating to $ENV EKS"
+                    
                 }
             }
-            stage('Sonar Check') {
-                steps {
-                    script { 
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarCheck()
-                    }
-                }
-            }
-           stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                    steps {
-                        sh 'echo Unit Test Cases Completed'
-                         }
-                    }
-                stage('Integration Tests') {
-                    steps {
-                        sh 'echo Integration Test Cases Completed'
-                         }
-                    }
-                stage('Functional Tests') {
-                    steps {
-                        sh 'echo Functional Test Cases Completed'
-                         }
-                    }
-                }
-            }
+
+
         }    // end of statges 
     }
 }
